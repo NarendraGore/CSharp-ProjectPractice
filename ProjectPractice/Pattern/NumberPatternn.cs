719d44e85e728282eddd445972b44c7cc95455cd
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:29:17 +0530

    Number Pattern Program

---
+++ ProjectPractice/Pattern/NumberPatternn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.Pattern
+{
+    internal class NumberPatternn
+    {
+        static void Main() {
+            int n = 5;
+            for (int i = 1; i <= 5; i++) {
+                for (int space = 1; space < i; space++) {
+                    Console.Write(" ");
+                
+                }
+                for (int j = i; j <= 5; j++) {
+
+
+                    Console.Write((j % 2 == 0) ? "2" : "1");
+
+                }
+                Console.WriteLine();    
+            
+            }
+        
+        }
+    }
+}
