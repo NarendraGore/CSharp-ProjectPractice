8fa1aa2e01cb80af483d0db32bb1faad6a7733ab
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:28:51 +0530

    Student max mark checking program

---
+++ ProjectPractice/OOP/StudentMarks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP
+{
+    internal class StudentMarks
+    {
+        private int stdRNo;
+        private string stdName;
+        internal int stdMarks;
+
+        internal void Accept(int stdRNo, string stdName, int stdMarks) {
+            this.stdRNo = stdRNo;
+            this.stdName = stdName;
+            this.stdMarks = stdMarks;
+
+        }
+        internal void Display() {
+            Console.WriteLine("Student Roll No is :{0}",stdRNo);
+            Console.WriteLine("Student Name is:{0}",stdName);
+            Console.WriteLine("Student Marks are:{0}",stdMarks);
+        }
+    }
+}
