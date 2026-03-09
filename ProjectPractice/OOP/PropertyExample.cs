81d9410b905a82430415e1c205b5cd0b7c0320dd
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:28:26 +0530

    Property Example

---
+++ ProjectPractice/OOP/PropertyExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP
+{
+    internal class PropertyExample
+    {
+        private int width;
+
+        public int Width { 
+            get { return width; } 
+            set { width = value; }
+        }
+    }
+}
