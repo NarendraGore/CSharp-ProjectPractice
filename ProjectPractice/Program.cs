46cf0aa46d4257f604cff1469f7efe08a4c7ac5a
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:30:05 +0530

    Main class

--- ProjectPractice/Program.cs
+++ ProjectPractice/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Transactions;
+using ProjectPractice.OOP;
 
 namespace ProjectPractice {
 
@@ -7,6 +9,7 @@ namespace ProjectPractice {
 
         static void Main(string[] args) {
 
+           
             //Hello obj = new Hello();
             //obj.Accept();
             //obj.Add();
@@ -57,18 +60,96 @@ namespace ProjectPractice {
 
             //Boxing Example
 
-              BoxingExample example = new BoxingExample ();
-            example.boxingfun();
-            example.Unboxingfun();
+            //  BoxingExample example = new BoxingExample ();
+            //example.boxingfun();
+            //example.Unboxingfun();
 
             //Data Types
 
-            DataType data = new DataType ();
+            //DataType data = new DataType ();
 
-            data.PDataType();
-            data.DDataType();
+            //data.PDataType();
+            //data.DDataType();
 
-    
+
+            /* //Employee Class Access
+                       Class_Obj emp = new Class_Obj ();
+                        emp.Accept(1,"ABC",40000);
+                        emp.Display();
+
+                        Class_Obj emp1 = new Class_Obj ();
+                        emp.Accept(2, "DEF", 45000);
+                        emp.Display();*/
+
+            /*Student Data Display
+
+
+            StudentMarks[] std = new StudentMarks[2];
+            for (int i = 0; i < std.Length; i++) {
+                std[i] = new StudentMarks();
+                Console.Write("Enter Roll Number:");
+                int rno = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Name:");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.Write("Enter Marks:");
+                int marks = Convert.ToInt32(Console.ReadLine());
+                std[i].Accept(rno, name, marks);
+            }
+           
+           
+            int max = 0;
+            int stdIndex = 0;
+            for (int i = 0; i < std.Length; i++) {
+                if (max < std[i].stdMarks)
+                {
+                    max= std[i].stdMarks;
+                    stdIndex = i;
+                }
+            
+            }
+            Console.WriteLine("Maximum Marks are:{0}", max);
+            std[stdIndex].Display();*/
+
+
+            /*Member Function Types
+
+            MemberFunction obj1=  new MemberFunction();
+            obj1.fun();
+            Console.WriteLine(obj1.fun2());
+            obj1.fun3(5);
+            Console.WriteLine(obj1.fun4(6));
+
+            */
+
+            /*Constructor Example
+             
+
+            ConstructorExample example = new ConstructorExample(10, 20);
+            ConstructorExample example1 = new ConstructorExample(example);
+            */
+            /*Property Example 
+            PropertyExample example = new PropertyExample();
+            example.Width = 100;
+            Console.WriteLine("Property example:"+example.Width);*/
+
+            /*OverLoading Example | PolyMorphism
+
+            OverLoadingExample example = new OverLoadingExample();
+            example.Add(1, 2);
+            example.Add(2, 3,5);
+            example.Add(2.15f, 22.85f);
+            */
+
+            /*Inheritance Example | Single | Multilevel | Hirarchical*/
+
+            B obj1 = new B();
+            obj1.fun2();
+            obj1.fun1();
+
+            C obj2 = new C();
+            obj2.fun3();
+            obj2.fun2();
+            obj2.fun1();
         }
 
     }
