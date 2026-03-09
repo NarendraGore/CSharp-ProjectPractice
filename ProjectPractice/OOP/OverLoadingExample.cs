4fab97b48d44f6f0059dc38a324fc743336ddb5e
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:28:09 +0530

    OverLoading Example

---
+++ ProjectPractice/OOP/OverLoadingExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP
+{
+    internal class OverLoadingExample
+    {
+        internal void Add(int a, int b) {
+
+            Console.WriteLine(a + b);
+
+        }
+        internal void Add(int a, int b, int c)
+        {
+
+            Console.WriteLine(a + b+c);
+
+        }
+        internal void Add(float a, float b)
+        {
+
+            Console.WriteLine(a + b);
+
+        }
+
+    }
+}
