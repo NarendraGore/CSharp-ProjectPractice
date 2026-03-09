ee4e033958c796c6a569f3c84279c84f260e77f6
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:29:07 +0530

    Char pattern program

---
+++ ProjectPractice/Pattern/CharPattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.Pattern
+{
+    internal class CharPattern
+    {
+        static void Main() {
+            /*
+             MANISH
+              ANISH
+               NISH
+                ISH
+                 SH
+                  H*/
+
+            string str = "MANISH";
+
+            for (int i = 0; i < str.Length; i++) {
+
+                for (int space = 0; space < i; space++) {
+                    Console.Write(" ");
+                }	
+
+                for (int j = i; j < str.Length; j++) {
+                    Console.Write(str[j]);
+                }
+                Console.WriteLine();
+            
+            }
+        
+        }
+    }
+}
