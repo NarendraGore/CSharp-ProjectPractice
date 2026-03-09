f5f004f624d4eeb3e7e8ea5fd3ec13a0f07a7abe
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:24:04 +0530

    Duplicate Check in Array

---
+++ ProjectPractice/Array/DuplicateCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.Array
+{
+    internal class DuplicateCheck
+    {
+        static void Main() {
+
+            int[] arr = { 1, 5, 2, 1, 3, 2,5,7,9,8,7,9,8 };
+            Console.Write("Duplicates Element are:");
+
+            for (int i = 0; i < arr.Length; i++) {
+                for (int j = i + 1; j < arr.Length; j++) {
+
+                    if (arr[i] == arr[j]) {
+                        
+                        Console.Write(arr[i]+", ");
+                        break;
+                    
+                    }
+                }
+            
+            }
+        }
+    }
+}
