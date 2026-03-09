46a4e9565c218c86c53b5009d83554cbd0ae7b86
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:25:38 +0530

    Find Number in array

---
+++ ProjectPractice/Array/NumberCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.Array
+{
+    internal class NumberCheck
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 12, 15, 30, 45, 69, 56, 56, 0, 65, 0, 560, 65, 6, 5, 56, 65 };
+
+            int number;
+            Console.WriteLine("Enter a Number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            bool isnumber = false; 
+
+            for (int i = 0; i < arr.Length; i++) {
+
+                if (arr[i] == number)
+                {
+
+                    Console.WriteLine("number is found at index:"+i);
+                    isnumber = true;
+                  
+                }
+              
+            
+            }
+
+            if (isnumber == false) {
+                Console.WriteLine("Number is not found in array");
+            }
+           
+            
+            }
+    }
+}
