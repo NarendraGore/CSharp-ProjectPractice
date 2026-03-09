b50ae0709a0a04b7c509b0f6565fa0096ccd16ec
NarendraGore <ngore4981@gmail.com>
2026-03-09 18:27:36 +0530

    Class object

---
+++ ProjectPractice/OOP/Class_Obj.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP
+{
+    internal class Class_Obj
+    {
+        private int empid;
+        private string empName;
+        private int empSalary;
+
+        internal void Accept(int empid, string empName,int empSalary) {
+        
+
+            this.empid = empid;
+            this.empName = empName;
+            this.empSalary = empSalary;
+        }
+        internal void Display() {
+
+            Console.WriteLine("Employee Id:{0}",empid);
+            Console.WriteLine("Employee Name:{0}", empName);
+            Console.WriteLine("Employee Salary:{0}",empSalary);
+        }
+    }
+}
