a1a265befeeae041968d6cf0360b06f2c4e1ccfc
NarendraGore <ngore4981@gmail.com>
2026-03-10 18:02:44 +0530

    Overhiding

---
+++ ProjectPractice/OOP/Overhiding/Bill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP.Overhiding
+{
+    internal class Bill
+    {
+        public double CalcBill(int amount) {
+            return amount;
+        }
+    }
+}
---
+++ ProjectPractice/OOP/Overhiding/GSTBill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP.Overhiding
+{
+    internal class GSTBill:Bill
+    {
+        public double CalcBill(int amount) {
+            return amount + amount * 0.18f;
+        }
+    }
+}
