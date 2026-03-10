0d8157c19d71d7c6cfe0c632bdabe45b020f3412
NarendraGore <ngore4981@gmail.com>
2026-03-10 18:01:29 +0530

    Access Specifiers

---
+++ ProjectPractice/OOP/AccessSpecifiers/AccessSpecifiers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP.AccessSpecifiers
+{
+    internal class AccessSpecifiers
+    {
+        private void fun1() {
+
+            Console.WriteLine("Private fun1");
+        
+        }
+        internal void fun2() {
+            Console.WriteLine("Internal fun2");
+
+        }
+
+        protected void fun3() {
+            Console.WriteLine("Protected fun3");
+
+        }
+
+        protected internal void fun4() {
+            Console.WriteLine("Protected Internal fun4");
+
+
+        }
+
+        public void fun5() {
+            Console.WriteLine("Public fun5");
+
+
+        }
+
+       
+    }
+}
