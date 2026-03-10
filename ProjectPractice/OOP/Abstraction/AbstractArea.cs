c4a0aead6a45f807d26af73078e8a46a65549046
NarendraGore <ngore4981@gmail.com>
2026-03-10 18:01:07 +0530

    Abstract class

---
+++ ProjectPractice/OOP/Abstraction/AbstractArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP.Abstraction
+{
+    internal abstract class AbstractArea
+    {
+        internal int param1;
+        internal int param2;
+        internal int area;
+
+        internal void Accept(int param1, int param2) { 
+            this.param1 = param1;
+            this.param2 = param2;
+        }
+
+        internal abstract void triangle();
+        internal abstract void rectangle();
+
+        internal void Display() {
+
+            Console.WriteLine("Area is {0}",area);
+        }
+    }
+}
---
+++ ProjectPractice/OOP/Abstraction/AbstractChildArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP.Abstraction
+{
+    internal class AbstractChildArea:AbstractArea
+    {
+        internal override void triangle()
+        {
+            area = (param1 * param2) / 2;
+        }
+        internal override void rectangle()
+        {
+            area = param1 * param2;
+        }
+    }
+}
