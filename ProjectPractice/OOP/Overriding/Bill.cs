0721b3df98954416d66ccd21521b2e6e2073ed5e
NarendraGore <ngore4981@gmail.com>
2026-03-10 18:02:55 +0530

    Overriding

---
+++ ProjectPractice/OOP/Overriding/Bill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractice.OOP.Overriding
+{
+    internal class Bill
+    {
+        virtual public double CalcBill(int amount) {
+            return amount;
+        }
+    }
+}
